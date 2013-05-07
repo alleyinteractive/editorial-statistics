65b011eea35186a35f4a7726b2696036bad0f5f1
bcampeau <brad@alleyinteractive.com>
2013-05-07 19:48:38 -0400

    Finished reporting logic, added validation to the form

--- editorial-statistics.php
+++ editorial-statistics.php
@@ -25,11 +25,20 @@ class Editorial_Statistics {
 	/** @type array Available report columns */
 	public $report_columns = array( 'author', 'content_type', 'term' );
 	
+	/** @type array Available report columns */
+	public $filtered_post_types = array( 'attachment', 'guest-author' );
+	
+	/** @type string Default date format to use for reports and date fields */
+	public $default_date_format = 'm/d/Y';
+	
 	/** @type array Predefined date ranges */
-	public $date_ranges = array( 'Yesterday', 'Week to Date', 'This Month', 'Last Month' );
+	public $date_ranges = array();
 	
 	/** @type string Screen ID */
 	private $screen_id = 'tools_page_editorial-statistics'; 
+	
+	/** @type object Dependency check on Co-Authors Plus **/
+	private $coauthors_plus;
 	
 
 	/**
@@ -76,22 +85,7 @@ class Editorial_Statistics {
 	 * @return void
 	 */
 	public function prepare() {
-		register_activation_hook( __FILE__, array( $this, 'dependencies' ) );
 		add_action( 'init', array( &$this, 'setup_plugin' ) );
-	}
-	
-	
-	/**
-	 * Handle plugin dependencies on activation
-	 *
-	 * @return void
-	 */
-	function dependencies() {
-		$coauthors_plus_dependency = new Plugin_Dependency( $this->plugin_name, 'Co-Authors Plus', 'http://wordpress.org/extend/plugins/co-authors-plus/' );
-		if( !$coauthors_plus_dependency->verify() ) {
-			// Cease activation
-			die( $coauthors_plus_dependency->message() );
-		}
 	}
 
 
@@ -102,8 +96,36 @@ class Editorial_Statistics {
 	 * @return void
 	 */
 	public function setup_plugin() {
+		// Add action hooks to display the report interface
 		add_action( 'admin_menu', array( &$this, 'register_management_page' ) );
 		add_action( 'admin_enqueue_scripts', array( &$this, 'enqueue_scripts' ) );
+		
+		// Add the date ranges
+		$this->date_ranges = array( 
+			array( 
+				'name' => 'Yesterday', 
+				'start_date' => date( $this->default_date_format, strtotime( 'yesterday' ) ),
+				'end_date' => date( $this->default_date_format, strtotime( 'yesterday' ) )
+			),
+			array(
+				'name' => 'Week to Date',
+				'start_date' => date( $this->default_date_format, strtotime( 'last monday' ) ),
+				'end_date' => date( $this->default_date_format )
+			),
+			array(
+				'name' => 'This Month',
+				'start_date' => date( $this->default_date_format, strtotime( 'first day of this month' ) ),
+				'end_date' => date( $this->default_date_format, strtotime( 'last day of this month' ) )
+			),
+			array(
+				'name' => 'Last Month',
+				'start_date' => date( $this->default_date_format, strtotime( 'first day of last month' ) ),
+				'end_date' => date( $this->default_date_format, strtotime( 'last day of last month' ) )
+			) 
+		);
+		
+		// Add the dependency check used later to check if Co-Authors Plus is active
+		$this->coauthors_plus = new Plugin_Dependency( $this->plugin_name, 'Co-Authors Plus', 'http://wordpress.org/extend/plugins/co-authors-plus/' );
 	}
 
 
@@ -128,6 +150,9 @@ class Editorial_Statistics {
 			// Add chosen.js for the taxonomy selection field
 			wp_enqueue_script( 'chosen', plugin_dir_url( __FILE__ ) . 'js/chosen/chosen.jquery.min.js', false, '1.0', true );
 			wp_enqueue_style( 'chosen_css', plugin_dir_url( __FILE__ ) . 'js/chosen/chosen.css', false, '1.0' );
+			
+			// Add form validation
+			wp_enqueue_script( 'jquery-validate', plugin_dir_url( __FILE__ ) . 'js/jquery.validate.min.js', false, '1.0', true );
 		
 			// Enqueue the plugin styles
 			wp_enqueue_style( $this->prefix . 'css', plugin_dir_url( __FILE__ ) . 'css/editorial-statistics.css', false, '1.0' );
@@ -161,11 +186,10 @@ class Editorial_Statistics {
 			<div class="wrap">
 				<?php screen_icon(); ?>
 				<h2><?php _e( $this->plugin_name, $this->i18n ) ?></h2>
-	
-				<form method="post" action="">
-					
+				
+				<form id="editorial_statistics_form" method="post" action="">	
+					<h3><?php _e( 'Report Settings', $this->i18n ) ?></h3>
 					<?php wp_nonce_field( $this->prefix . 'nonce' ) ?>
-			
 					<table class="form-table">
 						<tr valign="top">
 							<th scope="row">
@@ -173,14 +197,12 @@ class Editorial_Statistics {
 									<div>
 										<b><?php _e( 'Start Date', $this->i18n ) ?></b>
 									</div>
-									<div><?php _e( 'Choose the start date for the report.', $this->i18n ) ?></div>
+									<div><?php _e( 'Set the start date for the report.', $this->i18n ) ?></div>
 								</label>
 							</th>
 							<td>
-								<?php 
-									// TODO - some logic here to select date when report page refreshes 
-								?>
-								<input type="text" name="<?php echo $this->prefix ?>start_date" id="<?php echo $this->prefix ?>start_date" />
+								<input type="text" name="<?php echo $this->prefix ?>start_date" id="<?php echo $this->prefix ?>start_date" value="<?php echo ( isset( $_POST[$this->prefix . 'start_date'] ) ) ? $_POST[$this->prefix . 'start_date'] : '' ?>" />
+								<div class="error-message"></div>
 							</td>
 						</tr>
 						<tr valign="top">
@@ -189,14 +211,12 @@ class Editorial_Statistics {
 									<div>
 										<b><?php _e( 'End Date', $this->i18n ) ?></b>
 									</div>
-									<div><?php _e( 'Choose the end date for the report.', $this->i18n ) ?></div>
+									<div><?php _e( 'Set the end date for the report.', $this->i18n ) ?></div>
 								</label>
 							</th>
 							<td>
-								<?php 
-									// TODO - some logic here to select date when report page refreshes 
-								?>
-								<input type="text" name="<?php echo $this->prefix ?>end_date" id="<?php echo $this->prefix ?>end_date" />
+								<input type="text" name="<?php echo $this->prefix ?>end_date" id="<?php echo $this->prefix ?>end_date" value="<?php echo ( isset( $_POST[$this->prefix . 'end_date'] ) ) ? $_POST[$this->prefix . 'end_date'] : '' ?>" />
+								<div class="error-message"></div>
 							</td>
 						</tr>
 						<tr valign="top">
@@ -205,7 +225,7 @@ class Editorial_Statistics {
 									<div>
 										<b><?php _e( 'Predefined Date Ranges', $this->i18n ) ?></b>
 									</div>
-									<div><?php _e( 'Choose a predefined date range for the report.', $this->i18n ) ?></div>
+									<div><?php _e( 'Choose a predefined date range to set the start and end dates for the report.', $this->i18n ) ?></div>
 								</label>
 							</th>
 							<td>
@@ -213,9 +233,10 @@ class Editorial_Statistics {
 									$date_range_output = array();
 									foreach( $this->date_ranges as $date_range ) {
 										$date_range_output[] = sprintf(
-											'<a href="#" class="editorial-statistics-date-range" data-range="%s">%s</a>',
-											strtolower( str_replace( ' ', '_', $date_range ) ),
-											__( $date_range, $this->i18n )
+											'<a href="#" class="editorial-statistics-date-range" data-start-date="%s" data-end-date="%s">%s</a>',
+											$date_range['start_date'],
+											$date_range['end_date'],
+											__( $date_range['name'], $this->i18n )
 										);
 									}
 									echo implode( '&nbsp;&nbsp;|&nbsp;&nbsp;', $date_range_output );
@@ -234,19 +255,18 @@ class Editorial_Statistics {
 							<td>
 								<?php foreach( $this->report_columns as $report_column ): ?>
 									<?php 
-										// TODO - test logic here to select checkboxes when the report page refreshes to load the report
 										echo sprintf(
-											'<input type="checkbox" value="%s" name="%sreport_columns[]" id="%sreport_columns_%s" class="postform" %s />',
+											'<input type="checkbox" value="%s" name="%sreport_columns[]" id="%sreport_columns_%s" class="editorial-statistics-report-column" %s />',
 											$report_column,
 											$this->prefix,
 											$this->prefix,
 											$report_column,
-											( isset( $_POST[$this->prefix . '_report_columns'] ) && in_array( $report_columm, $_POST[$this->prefix . '_report_columns'] ) ) ? ' checked="checked"' : ''
+											( isset( $_POST[$this->prefix . 'report_columns'] ) && in_array( $report_column, $_POST[$this->prefix . 'report_columns'] ) ) ? ' checked="checked"' : ''
 										);
 									?>
-									<?php _e( ucwords( str_replace( '_', ' ', $report_column ) ), $this->i18n ) ?>
-									<br />
+									<?php _e( ucwords( str_replace( '_', ' ', $report_column ) ), $this->i18n ) ?><br />
 								<?php endforeach; ?>
+								<div class="error-message"></div>
 							</td>
 						</tr>
 						<tr valign="top" id="<?php echo $this->prefix ?>terms_wrapper" class="editorial-statistics-filter" >
@@ -262,21 +282,37 @@ class Editorial_Statistics {
 								<?php 
 									echo sprintf(
 										'<select multiple="multiple" class="chzn-select" name="%s" id="%s" data-placeholder="%s">%s</select>',
-										$this->prefix . 'terms',
+										$this->prefix . 'terms[]',
 										$this->prefix . 'terms',
 										__( 'Select Taxonomies' ),
 										$this->get_taxonomy_options()
 									);
 								?>
+								<div class="error-message"></div>
 							</td>
 						</tr>
 					</table>
 	
 					<p class="submit">
 						<?php submit_button( __( 'Create Report' ), 'primary', $this->prefix . 'submit', false ); ?>
+						<input type="button" name="editorial_statistics_reset" id="editorial_statistics_reset" class="button delete" value="<?php _e( 'Reset Options', $this->i18n ) ?>" />
 					</p>
 	
 				</form>
+				<?php
+				
+					// If the form was submitted, display the report
+					if ( ! empty( $_POST['editorial_statistics_submit'] ) ) {
+						// Capability check
+						if ( ! current_user_can( 'edit_others_posts' ) )
+							wp_die( __( 'You do not have access to perform this action' ) );
+			
+						// Form nonce check
+						check_admin_referer( $this->prefix . 'nonce' );
+						
+						$this->create_report();
+					}
+				?>
 			</div>
 		<?php
 	}
@@ -302,31 +338,12 @@ class Editorial_Statistics {
 			$taxonomy_options .= sprintf(
 				'<option value="%s" %s>%s</option>',
 				$taxonomy->name,
-				'', // TODO - add logic to set selected when page refreshes to load report
+				( isset( $_POST[$this->prefix . 'terms'] ) && in_array( $taxonomy->name, $_POST[$this->prefix . 'terms'] ) ) ? ' selected="selected"' : '',
 				$taxonomy->label
 			);
 		}
 		
 		return $taxonomy_options;
-	}
-
-
-	/**
-	 * Get the list of post types for use in filtering reports
-	 *
-	 * @access private
-	 * @return array
-	 */
-	private function get_post_types() {
-		// Only return post types that are shown in the admin interface.
-		// Otherwise, the list could be confusing to editors or provide invalid options.
-		return get_post_types(
-			array( 
-				'public' => true,
-				'show_ui' => true
-			),
-			'objects'
-		);
 	}
 
 
@@ -350,7 +367,47 @@ class Editorial_Statistics {
 	
 	
 	/**
-	 * Get the list of authors for use in filtering reports
+	 * Get the list of post types for use in filtering reports
+	 *
+	 * @access private
+	 * @return array
+	 */
+	private function get_post_types() {
+		// Only return post types that are shown in the admin interface.
+		// Otherwise, the list could be confusing to editors or provide invalid options.
+		$post_types = get_post_types(
+			array( 
+				'public' => true,
+				'show_ui' => true
+			),
+			'names'
+		);
+		
+		// Also remove unwanted post types from this list
+		$post_types = array_filter( $post_types, array( &$this, 'filter_post_types' ) );
+		
+		// Return the post types to be used for the report
+		return $post_types;
+	}
+	
+	
+	/**
+	 * Filters unwanted post types for the report
+	 *
+	 * @access private
+	 * @param string $post_type
+	 * @return array
+	 */
+	private function filter_post_types( $post_type ) {
+		// Apply filters to allow the list of filtered post types to be editable
+		$post_types_to_filter = apply_filters( $this->prefix . 'filtered_post_types', $this->filtered_post_types );
+		
+		return ! in_array( $post_type, $post_types_to_filter );
+	}
+	
+	
+	/**
+	 * DEPRECATED? Get the list of authors for use in filtering reports
 	 *
 	 * @access private
 	 * @return array
@@ -368,13 +425,236 @@ class Editorial_Statistics {
 		);
 	}
 	
+	
+	/**
+	 * Create the output for the editorial statistics report
+	 *
+	 * @access private
+	 * @return void
+	 */
+	private function create_report() {
+		// Query for all posts for public post types in the specified date range
+		add_filter( 'posts_where', array( &$this, 'report_date_filter' ) );
+		$args = array(
+			'post_type' => $this->get_post_types(),
+			'posts_per_page' => -1
+		);
+		$posts_query = new WP_Query( $args );
+		$posts = $posts_query->get_posts();		
+		remove_filter( 'posts_where', array( &$this, 'report_date_filter' ) );
+		
+		?>
+			<h3><?php _e( 'Report', $this->i18n ) ?></h3>
+		<?php
+			if ( count( $posts ) > 0 && isset( $_POST[$this->prefix . 'report_columns'] ) ) {
+		?>
+			<table>
+			<?php
+				// Create an array to hold the final data
+				$report_data = array();
+				
+				// Now we will iterate over each post. 
+				// The available report columns are author, content type, and tag in that order. 
+				// Based on which were selected, we will group counts for each into a multidimensional array
+				// that will later serve as the final output of the report. 
+				foreach( $posts as $post ) {
+					// Build the array keys that will be used to classify and count this post
+					// Report granularity should always be displayed as author, then content type and then term for the specified taxonomies
+					$keys = array();
+				
+					if ( in_array( 'author', $_POST[$this->prefix . 'report_columns'] ) ) {
+						$authors = array();
+						if ( $this->coauthors_plus->verify() ) {
+							foreach( get_coauthors( $post->ID ) as $coauthor ) {
+								$authors[] = $coauthor->display_name;
+							}
+						} else { 
+							$authors[] = $post->post_author;
+						}
+
+						$keys[] = $authors;
+					}
+					
+					if ( in_array( 'content_type', $_POST[$this->prefix . 'report_columns'] ) ) {
+						// Use an array here to be consistent with authors and terms and simplify the recursive function that adds totals
+						$keys[] = array( get_post_type_object( $post->post_type )->labels->singular_name );
+					}
+					
+					if ( in_array( 'term', $_POST[$this->prefix . 'report_columns'] ) ) {
+						$taxonomy_terms = array();
+						foreach( wp_get_post_terms( $post->ID, $_POST[$this->prefix . 'terms'] ) as $term ) {
+							$taxonomy_terms[] = $term->name;
+						}
+						
+						// If there are no terms, just use 'None'
+						if ( empty( $taxonomy_terms ) )
+							$taxonomy_terms[] = __( 'None', $this->i18n );
+						
+						$keys[] = $taxonomy_terms;
+					}
+
+					// Add this story to the totals for the appropriate rows in the final report
+					$report_data = $this->add_report_totals( $report_data, $keys );
+				}
+				
+				// Sort the data for the report
+				$this->sort_report_data( $report_data );
+				
+				// Output the data
+				?>
+				<table id="report_table">
+					<tr>
+						<?php	
+							foreach( $_POST[$this->prefix . 'report_columns'] as $report_column ) {
+								echo sprintf(
+									'<td class="header">%s</td>',
+									__( ucwords( str_replace( '_', ' ', $report_column ) ), $this->i18n )
+								);
+							}
+						?>
+						<td class="header"><?php _e( 'Total Stories', $this->i18n ) ?></td>
+					</tr>
+				<?php $this->output_report_data( $report_data ) ?>
+				</table>
+				<?php
+			?>
+			</table>
+		<?php } else { ?>
+			<h4><?php _e( 'No results were returned for the current report settings.', $this->i18n ) ?></h4>
+		<?php }
+	}
+	
+	
+	/**
+	 * Adds a date filter for the report
+	 *
+	 * @access public
+	 * @param string $where
+	 * @return string
+	 */
+	public function report_date_filter( $where = '' ) {
+		// Ensure the date parameters are set. Otherwise invalidate the query.
+		if( isset( $_POST[$this->prefix . 'start_date'] ) && isset( $_POST[$this->prefix . 'end_date'] ) )
+			$where .= " AND post_date >= '" . date( 'Y-m-d', strtotime( $_POST[$this->prefix . 'start_date'] ) ) . "' AND post_date < '" . date( 'Y-m-d', strtotime( $_POST[$this->prefix . 'end_date'] ) + ( 60*60*24 ) ) . "'";
+		else
+			$where .= " AND 1=2";
+			
+		return $where;
+	}
+	
+	
+	/**
+	 * Recursive function to add to the totals for the final report
+	 * Adds one to the lowest level of the indexes provided
+	 *
+	 * @access private
+	 * @param array $report_data
+	 * @param array $keys
+	 * @return string
+	 */
+	private function add_report_totals( $report_data, $keys ) {
+		// Get the keys used for this level and shift them off the array of keys
+		$lvl_keys = array_shift( $keys );
+		
+		// Iterate through the keys for this level of the report
+		foreach( $lvl_keys as $lvl_key ) {
+			// If the keys array is now empty, we have reached the lowest level and add one to the key
+			// Otherwise, recurse with the trimmed keys array and set this index equal to the result
+			if ( empty( $keys ) ) {
+				$report_data[$lvl_key]++;
+			} else {
+				// If this key does not yet exist, initialize it as an empty array
+				if ( ! array_key_exists( $lvl_key, $report_data ) )
+					$report_data[$lvl_key] = array();
+				
+				// Recurse with the newly trimmed keys array to get down to the final level to add totals
+				$report_data[$lvl_key] = $this->add_report_totals( $report_data[$lvl_key], $keys );
+			}
+		}
+		
+		// Return the final report data
+		return $report_data;
+	}
+	
+	
+	/**
+	 * Recursive function to output the final data for the report
+	 * Uses a simple HTML table or CSV format
+	 *
+	 * @access private
+	 * @param array $report_data
+	 * @param array $row_values
+	 * @param string $report_format
+	 * @return string
+	 */
+	private function output_report_data( $report_data, $row_values = array(), $report_format = 'HTML' ) {
+		// Determine if we are still building a row of data or if it is ready to be output
+		if ( is_array( $report_data ) ) {
+			// This is still an array of data so get the keys for the current level
+			// Add it to the row values as the next column of data to be output and pass its value to this function recursively
+			$keys = array_keys( $report_data );
+			
+			foreach( $keys as $key ) {
+				// Add this key as a column in the report
+				$row_values[] = $key;
+				
+				// Recursively call this function until we reach the total (i.e. final column)
+				$this->output_report_data( $report_data[$key], $row_values, $report_format );
+				
+				// Pop this column off the output data before we iterate to the next one
+				array_pop( $row_values );
+			}
+		} else {
+			// If report data is not an array, we've reached the lowest level of the report data and should output the row
+			// Set the row start, end and separators based on the output format
+			switch( $report_format ) {
+				case 'CSV':
+					$row_start = "'";
+					$row_end = "'\r";
+					$separator = "','";
+					break;
+				case 'HTML':
+				default:
+					$row_start = '<tr><td>';
+					$row_end = '</td></tr>';
+					$separator = '</td><td>';
+					break;
+			}
+			
+			// Start the row
+			echo $row_start;
+
+			// Output the row data after adding the final column, which is the total
+			$row_values[] = $report_data;
+			echo implode( $separator, $row_values );
+			
+			// End the row
+			echo $row_end;
+		}
+	}
+	
+	
+	/**
+	 * Recursive function to sort the data before displaying it
+	 *
+	 * @access private
+	 * @param array $report_data
+	 * @return string
+	 */
+	private function sort_report_data( &$report_data ) {
+		foreach( $report_data as $column ) {
+			if ( is_array( $column ) )
+				$this->sort_report_data( $column );
+		}
+		ksort( $report_data );
+	}
+	
 
 	/**
 	 * Log an error
 	 *
 	 * @param string $message 
 	 * @return bool false
-	 * @author Matthew Boynes
 	 */
 	private function error( $message ) {
 		$this->errors[] = $message;
--- js/editorial-statistics.js
+++ js/editorial-statistics.js
@@ -2,69 +2,83 @@
 
 	// Handles showing/hiding additional fields required for certain columns in the report
 	var editorial_statistics_show_field = function( $checkbox, $target ) {
-		if( $checkbox.attr( "checked" ) == "checked" )
+		if( $checkbox.attr( "checked" ) == 'checked' )
 			$target.show();
 		else
 			$target.hide();
 	}
 	
-	// Handles setting a predefined date range
-	var editorial_statistics_set_date_range = function( $start, $end, range ) {
-		var current_date, start_date, end_date;
-		
-		// Set the current date to use in date calculations
-		current_date = new Date();
-		
-		switch( range ) {
-			case 'yesterday':
-				console.log( current_date.getTime() );
-				start_date = end_date = new Date( current_date.getTime() - 60*60*24 );
-				break;
-			case 'week_to_date':
-			
-				break;
-			case 'this_month':
-			
-				break;
-			case 'last_month':
-			
-				break;
-			default:
-				// If for some reason this receives an invalid range, it will just do nothing but this should never be possible
-				return;
-				break; 
-		}
-		
-		$start.val( editorial_statistics_format_date( start_date.getMonth(), start_date.getDay(), start_date.getYear() ) );
-		$end.val( editorial_statistics_format_date( end_date.getMonth(), end_date.getDay(), end_date.getYear() ) );
+	// Handles resetting the entire form
+	var editorial_statistics_reset_options = function() {
+		$( '#' + editorial_statistics.prefix + 'start_date' ).val( '' );
+		$( '#' + editorial_statistics.prefix + 'end_date' ).val( '' );
+		$( '.editorial-statistics-report-column' ).removeAttr( 'checked' );
+		$( '#editorial_statistics_terms option:selected' ).removeAttr( 'selected' );
+		$( '#editorial_statistics_terms' ).trigger( 'liszt:updated' );
+		editorial_statistics_show_field( $( '#' + editorial_statistics.prefix + 'report_columns_term' ), $( '#' + editorial_statistics.prefix + 'terms_wrapper' ) );
 	}
 	
-	// Handles formatting a date
-	var editorial_statistics_format_date = function( month, day, year ) {
-		return month + "/" + day + "/" + year;
+	// Handles setting a predefined date range
+	var editorial_statistics_set_date_range = function( $range ) {
+		$( '#' + editorial_statistics.prefix + 'start_date' ).val( $range.data( 'startDate' ) );
+		$( '#' + editorial_statistics.prefix + 'end_date' ).val( $range.data( 'endDate' ) );
+		
+		// Also clear any error messages about start/end date since these are now set
+		$( '#' + editorial_statistics.prefix + 'start_date' ).siblings( '.error-message' ).html( '' );
+		$( '#' + editorial_statistics.prefix + 'end_date' ).siblings( '.error-message' ).html( '' );
 	}
 	
 	$( document ).ready( function() {
 	
 		// Initialize the datepickers
-		$( "#" + editorial_statistics.prefix + "start_date" ).datepicker();
-		$( "#" + editorial_statistics.prefix + "end_date" ).datepicker();
+		$( '#' + editorial_statistics.prefix + 'start_date' ).datepicker();
+		$( '#' + editorial_statistics.prefix + 'end_date' ).datepicker();
 		
 		// Initialize chosen for term selection
-		$( "#" + editorial_statistics.prefix + "terms" ).chosen();
+		$( '#' + editorial_statistics.prefix + 'terms' ).chosen();
+		
+		// Clear any error messages after fields are selected via chosen
+		$( '#' + editorial_statistics.prefix + 'terms' ).chosen().change( function() {
+			$( '#' + editorial_statistics.prefix + 'terms' ).siblings( '.error-message' ).html( '' );
+		} );
 		
 		// Handle displaying the taxonomy selection when Term is selected as a column
-		$( "#editorial_statistics_report_columns_term" ).on( "click", function( event ) {
-			editorial_statistics_show_field( $(this), $( "#" + editorial_statistics.prefix + "terms_wrapper" ) );
+		$( '#editorial_statistics_report_columns_term' ).on( 'click', function( event ) {
+			editorial_statistics_show_field( $(this), $( '#' + editorial_statistics.prefix + 'terms_wrapper' ) );
 		} );
 		
 		// Handle setting a predefined date range when a link is clicked
-		$( ".editorial-statistics-date-range" ).on( "click", function( event ) {
-			editorial_statistics_set_date_range( $( "#" + editorial_statistics.prefix + "start_date" ), $( "#" + editorial_statistics.prefix + "end_date" ), $(this).data( "range" ) );
+		$( '.editorial-statistics-date-range' ).on( 'click', function( event ) {
+			event.preventDefault();
+			editorial_statistics_set_date_range( $(this) );
+		} );
+
+		// Add jQuery form validation
+		$( '#editorial_statistics_form' ).validate({
+			groups: {
+				names: "editorial_statistics_report_columns[]"
+			},
+			rules: {
+				editorial_statistics_start_date: "required",
+				editorial_statistics_end_date: "required",
+				editorial_statistics_terms: {
+					required: "#editorial_statistics_report_columns_term:checked"
+				},
+				'editorial_statistics_report_columns[]': "required"
+			},
+			errorPlacement: function( error, element ) {
+				error.appendTo( element.siblings( '.error-message' ) );
+			},
+			ignore: ".default" // for chosen.js compatibility
+		});
+		
+		// Handle resetting all options on the form
+		$( '#editorial_statistics_reset' ).on( 'click', function( event ) {
+			editorial_statistics_reset_options();
 		} );
 		
 		// Determine whether or not to display the taxonomy selection field on load
-		editorial_statistics_show_field( $(this), $( "#" + editorial_statistics.prefix + "terms_wrapper" ) );
+		editorial_statistics_show_field( $( '#' + editorial_statistics.prefix + 'report_columns_term' ), $( '#' + editorial_statistics.prefix + 'terms_wrapper' ) );
 		
 	} );
 
